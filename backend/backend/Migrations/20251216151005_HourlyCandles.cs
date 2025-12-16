3e714e2703352ec1a4b939e283c491f434c17810
Lawrence Paine <60822247+LawrencePaine@users.noreply.github.com>
2025-12-16 15:11:41 +0000

    db updates

---
+++ backend/backend/Migrations/20251216151005_HourlyCandles.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class HourlyCandles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WinRatePct",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SharpeLike",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AvgTradePnlGbp",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WinRatePct",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SharpeLike",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AvgTradePnlGbp",
+                table: "BacktestMetrics",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
